18a7aa0986e3fb3726f914ab158b2ede31579084
xiaoxiao <593302270@qq.com>
2023-01-16 12:55:28 +0800

    Initial Commit

---
+++ CS193p/CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193pApp.swift
+//  CS193p
+//
+//  Created by apple on 2023/1/16.
+//
+
+import SwiftUI
+
+@main
+struct CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
